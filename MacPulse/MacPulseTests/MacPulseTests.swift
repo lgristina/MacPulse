3b03ea27a1989cf0c9fb031b157117ac032bb2a1
Luca Gristina <79229566+lgristina@users.noreply.github.com>
2025-03-10 20:50:56 -0400

    Initial Commit

---
+++ MacPulse/MacPulseTests/MacPulseTests.swift
@@ -0,0 +1,16 @@
+//
+//  MacPulseTests.swift
+//  MacPulseTests
+//
+//  Created by Luca Gristina on 3/10/25.
+//
+
+import Testing
+
+struct MacPulseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
