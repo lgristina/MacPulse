377aa81bc4cc8b760170e18b13304cf7e5d12699
afrank2002 <austinfrank00@gmail.com>
2025-04-20 14:45:20 -0400

    added test for main

--- MacPulse/MacPulseTests/MacPulseTests.swift
+++ MacPulse/MacPulseTests/MacPulseTests.swift
@@ -6,11 +6,22 @@
 //
 
 import Testing
+@testable import MacPulse
+import SwiftData
 
 struct MacPulseTests {
 
     @Test func example() async throws {
         // Write your test here and use APIs like `#expect(...)` to check expected conditions.
     }
+    
+    @Test func testModelContainerInitializes() throws {
+            let schema = Schema([SystemMetric.self, CustomProcessInfo.self])
+            let config = ModelConfiguration(schema: schema, isStoredInMemoryOnly: true) // use memory only for testing
+            
+            let container = try ModelContainer(for: schema, configurations: [config])
+            
+            #expect(container != nil)
+        }
 
 }
